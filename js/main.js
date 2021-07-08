12b1906e75fb5cd6c70f6ee5727ce417450ebb03
ke-yogi <ke-yogi@pdc.proto-g.co.jp>
2021-07-08 18:47:16 +0900

    リアルタイム結果表示構築

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ $(function () {
     var isTouchDevice = (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
     var eventType = (isTouchDevice) ? 'touchend' : 'click';
 
-    tabChange(); //タブ切り替え
-
     function tabChange(params) {
         $('.navList__item').on(eventType, function () {
             $('.navList__item').removeClass('current');
@@ -20,7 +18,27 @@ $(function () {
         });
     }
 
+    function realtimeResult() {
+        $('#page1 select, #page1 input').on('change', function () {
+            const player_index = $(this).closest('li').index();
 
+            if ($(this).is('select')) {
+                const name = $(this).val();
+                $('.inputRealtimeResultTable__name').eq(player_index).text(name);
+            }
+
+            if ($(this).is('input')) {
+                const point = $(this).val();
+                $('.inputRealtimeResultTable__point').eq(player_index).text(point);
+            }
+        });
+    };
+
+    tabChange(); //タブ切り替え
+
+    $(window).on('load', function (params) {
+        realtimeResult(); //DOMの監視
+    });
 
 
 });
